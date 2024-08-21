0aab81be66a4b830869325a8ab7bb068c35278e4
SAI DUTT <duttganduri46@gmail.com>
2024-08-20 17:39:43 -0700

    Initial Commit

---
+++ NurtureTrak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NurtureTrak
+//
+//  Created by Sai Dutt Ganduri on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NurtureTrak/NurtureTrakApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurtureTrakApp.swift
+//  NurtureTrak
+//
+//  Created by Sai Dutt Ganduri on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct NurtureTrakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
